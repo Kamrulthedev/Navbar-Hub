b0e1c77f8c6e39a0a482f55e14ba81e18c98bd02
Kamrul Hassan <kamrulthedev@gmail.com>
2025-03-10 20:08:23 +0600

    update-icon

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Responsive Navbar Hub" },
+    { name: "description", content: "Welcome Responsive Navbar Hub!" },
   ];
 }
 
