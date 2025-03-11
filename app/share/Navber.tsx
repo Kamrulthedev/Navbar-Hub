276e249947f42081bf4a194a8b5c958b82e529bd
Kamrulthedev <kamrulthedevR@gmail.com>
2025-03-12 00:25:26 +0600

    navbar-update-commit

--- app/share/Navber.tsx
+++ app/share/Navber.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     <nav className="bg-indigo-50 p-4 text-black">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         <h1 className="text-3xl font-serif">
-          <Link className="font-bold" to="/">Navbar Hub</Link>
+          <Link className="font-bold text-indigo-500" to="/">Navbar Hub</Link>
         </h1>
 
         {/* Desktop Menu */}
@@ -29,16 +29,20 @@ const Navbar = () => {
 
         {/* Mobile Menu Button */}
         <div className="lg:hidden flex items-center">
-          <button onClick={toggleMobileMenu} className="text-indigo-700">
+          <button
+            onClick={toggleMobileMenu}
+            className="text-indigo-700 hover:text-indigo-950 hover:shadow-2xl active:scale-90 transition-all duration-150 ease-out"
+          >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button>
+
         </div>
       </div>
 
       {/* Mobile Dropdown Menu with Animation */}
-      <div 
+      <div
         className={`lg:hidden transform transition-all duration-300 ${isMobileMenuOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0 overflow-hidden"}`}
       >
         <div className="relative">
