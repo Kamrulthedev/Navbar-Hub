1a1441f171ddab4824ab4ddad4516fa5dd17d792
Kamrulthedev <kamrulthedevR@gmail.com>
2025-03-12 01:14:22 +0600

    create-news-page

--- app/routes.ts
+++ app/routes.ts
@@ -3,5 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("page/home/Home.tsx"), 
     route("/about", "page/about/About.tsx"), 
-    // route("/contact", "routes/contact.tsx"), 
+    route("/news", "page/news/News.tsx"), 
 ] satisfies RouteConfig;
--- app/share/Navber.tsx
+++ app/share/Navber.tsx
@@ -19,6 +19,7 @@ const Navbar = () => {
         <div className="hidden lg:flex space-x-10 text-2xl font-serif">
           <a href="/" className="hover:text-indigo-500 flex gap-1"><span className="text-lg">Home</span></a>
           <a href="/about" className="hover:text-indigo-500 flex gap-1"><span className="text-lg">About</span></a>
+          <a href="/news" className="hover:text-indigo-500 flex gap-1"><span className="text-lg">News</span></a>
           <a href="#" className="hover:text-indigo-500 flex gap-1"><span className="text-lg">Services</span></a>
           <a href="#" className="hover:text-indigo-500 flex gap-1"><span className="text-lg">Contact</span></a>
         </div>
