ea1118a943dad2af830621e3df33c205801439f6
Kamrulthedev <kamrulthedevR@gmail.com>
2025-03-18 17:45:19 +0600

    update-code

--- app/page/contact/Contact.tsx
+++ app/page/contact/Contact.tsx
@@ -31,7 +31,6 @@ const Contact = () => {
             <label className="block text-gray-700 font-medium">Message</label>
             <textarea
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              rows="4"
               placeholder="Enter your message"
             ></textarea>
           </div>
