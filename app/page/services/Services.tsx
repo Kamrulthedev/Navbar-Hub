db1396caa25f328f94170997408c9d8a0c752365
Kamrulthedev <kamrulthedevR@gmail.com>
2025-03-19 21:26:55 +0600

    add -ankor-tag

--- app/page/services/Services.tsx
+++ app/page/services/Services.tsx
@@ -12,12 +12,12 @@ const Services = () => {
             <section></section>
             <div>
                 <nav className="bg-indigo-50 p-4 text-black">
-                     <div className='ml-10 p-1'>
+                     <div className='ml-10 p-1 flex justify-between items-center'>
                         <h1 className='text-3xl font-serif'>
                             <Link to=''>New Navber</Link>
                         </h1>
                         <div>
-
+                            <a href="">Home</a>
                         </div>
                      </div>
 
