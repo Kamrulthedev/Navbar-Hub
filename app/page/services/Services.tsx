9594ba9ba8c8b2d70410d2003e1ccf0ccf513d3b
Kamrulthedev <kamrulthedevR@gmail.com>
2025-03-20 21:28:06 +0600

    update-services

--- app/page/services/Services.tsx
+++ app/page/services/Services.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router';
+import { IoIosCloseCircle } from "react-icons/io";
+
 
 const Services = () => {
-      const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-    
-      const toggleMobileMenu = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen);
-      };
+    };
     return (
         <div className='mt-5'>
             <section></section>
             <div>
                 <nav className="bg-indigo-50 p-4 text-black">
-                     <div className='ml-10 p-1 flex justify-between items-center'>
+                    <div className='ml-10 p-1 flex justify-between items-center'>
                         <h1 className='text-3xl font-serif'>
                             <Link to=''>New Navber</Link>
                         </h1>
@@ -23,8 +25,15 @@ const Services = () => {
                             <a href="/" className='text-gray-500 hover:text-gray-700'>Notification</a>
                             <a href="/" className='text-gray-500 hover:text-gray-700'>Profile</a>
                         </div>
-                     </div>
+                    </div>
+                    {/* Mobaile Navber Animation  */}
+                    <div className={`fixed top-0 left-0 w-full h-96 bg-black/30 backdrop-blur-md flex justify-center transition-all duration-300 ${isMobileMenuOpen ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-full pointer-events-none"}`}>
+                        <div>
+                            <button>
 
+                            </button>
+                        </div>
+                    </div>
                 </nav>
             </div>
         </div>
