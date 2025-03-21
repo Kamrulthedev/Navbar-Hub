c9b4763e8b3f7c1904d1078a8e10dd6d4aa6b6ab
Kamrulthedev <kamrulthedevR@gmail.com>
2025-03-21 12:37:17 +0600

    create-new-for-loop

--- app/Js/loop.js
+++ app/Js/loop.js
@@ -1,5 +1,9 @@
 //for loop
 
-for(let i = 0; i < 3; i = i + 1){
-console.log(i)
-};
+// for(let i = 0; i < 3; i = i + 1){
+// console.log(i)
+// };
+
+
+
+for(let i = 0; i < 30; console.log(i = i + 5)) ;
