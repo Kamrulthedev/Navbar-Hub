9e0806fb47782f292b478a6a51b7b8efdae1e882
Kamrulthedev <kamrulthedevR@gmail.com>
2025-03-21 12:44:32 +0600

    sum-count-ok

--- app/Js/loop.js
+++ app/Js/loop.js
@@ -4,11 +4,8 @@
 // console.log(i)
 // };
 
-
-
 // for(let i = 0; i < 30; console.log(i = i + 5));
 
-
 let sum = 0;
-for (let i = 0; i < 100; sum = sum + i, i++)
-    console.log(sum)
+for (let i = 0; i <= 100; sum = sum + i, i++); 
+console.log(sum);
